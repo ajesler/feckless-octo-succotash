79fb1b7ce886016aa6db69d427657a88016d20b6
aj esler <andrew.esler@powershop.co.nz>
2015-12-17 16:40:33 +1300

    Back link shows in settings when accessed from popup.html

--- extension/js/options_init.js
+++ extension/js/options_init.js
@@ -1,8 +1,11 @@
 var storedState = localStorage.getItem('jenkins-config');
 var startingState = storedState ? JSON.parse(storedState) : null;
 
-var app = Elm.fullscreen(Elm.OptionsEditor, { getStorage: startingState });
+var app = Elm.fullscreen(Elm.OptionsEditor, {
+  getStorage: startingState,
+  locationSearch: window.location.search
+});
 
 app.ports.setStorage.subscribe(function(state) {
-    localStorage.setItem('jenkins-config', JSON.stringify(state));
+  localStorage.setItem('jenkins-config', JSON.stringify(state));
 });
