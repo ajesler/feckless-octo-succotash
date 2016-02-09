5587c4366deb051da02594e9ba0cc5f92d4fe26b
aj.esler <aj@esler.co.nz>
2016-02-09 20:50:37 +1300

    Build only completes when Elm source built successfully

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ cp manifest.json extension/manifest.json
 
 elm package install
 
-elm make src/BranchManager.elm --output extension/js/popup.js
-elm make src/OptionsEditor.elm --output extension/js/options.js
-
+elm make src/BranchManager.elm --output extension/js/popup.js &&
+elm make src/OptionsEditor.elm --output extension/js/options.js &&
 echo "Package build complete in extension/"
