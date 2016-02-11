3db8b29c6e15b2d92aec2343c5b7d73dffdde261
aj.esler <aj@esler.co.nz>
2016-02-11 21:06:54 +1300

    Focus on branch name when popup is loaded

--- assets/js/popup_init.js
+++ assets/js/popup_init.js
@@ -2,3 +2,12 @@ var storedState = localStorage.getItem('jenkins-config');
 var startingState = storedState ? JSON.parse(storedState) : null;
 
 var app = Elm.fullscreen(Elm.BranchManager, { getStorage: startingState });
+
+function focusOnBranchName() {
+  var input = document.getElementById('branchName');
+  if ( input != null && document.activeElement !== input ) {
+    input.focus();
+  }
+}
+
+setTimeout(focusOnBranchName, 50);
